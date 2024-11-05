c67f11e6480aa8ffede32f74a0ff10bb7fb080a8
John Lyndo Anuada <157618361+anuadajohnlyndo@users.noreply.github.com>
2024-11-05 18:38:26 +0800

    upload new files

---
+++ index.js
@@ -0,0 +1,21 @@
+setScreenSize();
+loadHomepage();
+
+
+
+
+
+
+
+function loadHomepage() {
+    window.addEventListener("load", function () {
+        document.getElementById("loading_animation_div").style.display = "none";
+        this.document.body.style.display = "flex";
+    });
+}
+
+
+function setScreenSize() {
+    document.body.style.width = window.innerWidth + "px";
+    document.body.style.height = window.innerHeight + "px";
+}
