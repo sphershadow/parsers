a0d4e0f2adc390ef9a82f1ea0bb97de7aa462370
John Lyndo Anuada <157618361+anuadajohnlyndo@users.noreply.github.com>
2024-11-06 10:40:06 +0800

    size

--- login.js
+++ login.js
@@ -1,10 +1,16 @@
 
 //tasks
 setScreenSize(window.innerWidth, window.innerHeight);
+setLoginBodyHeight(window.innerHeight);
 
 
 //functions
 function setScreenSize(width, height) {
     document.body.style.width = width + "px";
     document.body.style.height = height + "px";
+}
+
+function setLoginBodyHeight(height) {
+    const bodyheight = height - 210;
+    document.getElementById("body_login_div").style.height = bodyheight + "px";
 }
