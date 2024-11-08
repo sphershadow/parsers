8356ff59758fed460a324a7f2ffae946f3991b72
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-08 16:49:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ redirectParser();
 
 //functions
 function redirectParser() {
-    const parser_uid = localStorage.getItem("user-parser");
-    if (parser_uid == null) {
-        window.location.href = "login.html";
-    }
-    if (parser_uid != null) {
-        //window.location.href = "homepage.html";
-    }
+    window.location.href = "login.html";
+    // const parser_uid = localStorage.getItem("user-parser");
+    // if (parser_uid == null) {
+    //     window.location.href = "login.html";
+    // }
+    // if (parser_uid != null) {
+    //     //window.location.href = "homepage.html";
+    // }
 }
