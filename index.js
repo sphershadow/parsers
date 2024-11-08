622f211e2accdcf8803ce4adfebf5da0a4a1f187
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-08 16:48:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function redirectParser() {
         window.location.href = "login.html";
     }
     if (parser_uid != null) {
-        window.location.href = "homepage.html";
+        //window.location.href = "homepage.html";
     }
 }
