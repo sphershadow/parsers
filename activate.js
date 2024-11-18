475019544c3a845f503cf06a5474f08efb9f14bb
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-18 09:00:47 +0800

    Update activate.js

--- activate.js
+++ activate.js
@@ -116,9 +116,7 @@ document.getElementById("step5_btn").addEventListener("click", (event) => {
 
 document.getElementById('birthday').addEventListener('focus', function () {
     document.getElementById('dateInput').click();
-    document.getElementById('dateInput').addEventListener('change', function (event) {
-        document.getElementById('birthday').value = event.target.value;
-    });
+
 });
 
 
