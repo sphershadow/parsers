66bb223c15acb7aac05a144825ee1a607a10d0da
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-23 21:10:35 +0800

    update 11/23/24

--- activate.js
+++ activate.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import {
     getAuth,
-    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import {
     getDatabase,
@@ -9,7 +9,8 @@ import {
     get,
     child,
     update,
-    remove
+    remove,
+    set,
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 const firebaseConfig = {
     apiKey: "AIzaSyCFqgbA_t3EBVO21nW70umJOHX3UdRr9MY",
@@ -63,8 +64,17 @@ document.getElementById("cancel_btn").addEventListener("click", function () {
 //for activation
 document.getElementById("step1_btn").addEventListener("click", (event) => {
     event.preventDefault();
-    openSection(2);
-    hideSection(1, 3);
+    const username = document.getElementById("txtusername").value;
+    if (username === "") {
+        document.getElementById("txtusername").style.border = "1px solid red";
+        document.getElementById("txtusername").style.animation = "shake 0.3s ease-in-out";
+    }
+    else {
+        document.getElementById("txtusername").style.border = "1px solid #e0e0e0";
+        document.getElementById("txtusername").style.animation = "none";
+        checkUsername(username);
+    }
+
 });
 document.getElementById("step2_btn").addEventListener("click", (event) => {
     event.preventDefault();
@@ -73,27 +83,42 @@ document.getElementById("step2_btn").addEventListener("click", (event) => {
 });
 document.getElementById("step3_btn").addEventListener("click", (event) => {
     event.preventDefault();
-
+    const password = document.getElementById("txtpassword").value;
+    const confirm = document.getElementById("txtconfirmpass").value;
+    if (password === "") {
+        document.getElementById("txtpassword").style.border = "1px solid red";
+        document.getElementById("txtpassword").style.animation = "shake 0.3s ease-in-out";
+    }
+    else if (confirm === "") {
+        document.getElementById("txtconfirmpass").style.border = "1px solid red";
+        document.getElementById("txtconfirmpass").style.animation = "shake 0.3s ease-in-out";
+    }
+    else {
+        if (password.length < 8) {
+            document.getElementById("txtpassword").style.border = "1px solid red";
+            document.getElementById("txtpassword").style.animation = "shake 0.3s ease-in-out";
+            document.getElementById("txtconfirmpass").style.border = "1px solid #e0e0e0";
+            document.getElementById("txtconfirmpass").style.animation = "none";
+        }
+        else {
+            if (password === confirm) {
+                const username = document.getElementById("txtusername").value;
+                createParser(emailparser, password, id, username);
+            }
+            else {
+                document.getElementById("txtpassword").style.border = "1px solid red";
+                document.getElementById("txtpassword").style.animation = "shake 0.3s ease-in-out";
+                document.getElementById("txtconfirmpass").style.border = "1px solid red";
+                document.getElementById("txtconfirmpass").style.animation = "shake 0.3s ease-in-out";
+            }
+        }
+    }
 });
 
 editdetails_btn.addEventListener("click", (event) => {
     event.preventDefault();
     openSignup();
 });
-
-document.getElementById("step5_btn").addEventListener("click", (event) => {
-
-
-});
-
-document.getElementById('birthday').addEventListener('focus', function () {
-    document.getElementById('dateInput').click();
-});
-
-document.getElementById('dateInput').addEventListener('change', function (event) {
-    document.getElementById('birthday').value = event.target.value;
-});
-
 
 
 //functions
@@ -169,7 +194,6 @@ function updateDBVerification(id, code, type) {
 
 
 }
-
 
 function disableResend() {
     let countdownNumber = 30;
@@ -263,4 +287,113 @@ function hideSection(b, c) {
         stepCircle.style.backgroundColor = "#fefefe";
         stepCircle.style.color = "#007AFF";
     });
+}
+
+function checkUsername(username) {
+    get(child(dbRef, "PARSEIT/username/" + username)).then((snapshot) => {
+        if (snapshot.exists()) {
+            document.getElementById("txtusername").style.border = "1px solid red";
+            document.getElementById("txtusername").style.animation = "shake 0.3s ease-in-out";
+        }
+        else {
+            openSection(2);
+            hideSection(1, 3);
+            populateParser(localStorage.getItem("activate-parser"), username);
+        }
+    });
+}
+
+function populateParser(id, username) {
+    get(child(dbRef, "PARSEIT/administration/students/" + id)).then((snapshot) => {
+        if (snapshot.exists()) {
+            document.getElementById("confirm-id").innerText = snapshot.val().id;
+            document.getElementById("confirm-fullname").innerText = localStorage.getItem("name-parser");
+            document.getElementById("confirm-username").innerText = username;
+            document.getElementById("confirm-birthday").innerText = snapshot.val().birthday;
+            document.getElementById("confirm-email").innerText = localStorage.getItem("email-parser");
+            document.getElementById("confirm-yrlvl").innerText = getYearType(snapshot.val().yearlvl);
+            document.getElementById("confirm-section").innerText = snapshot.val().section;
+            document.getElementById("confirm-regular").innerText = "(" + checkRegular(snapshot.val().regular) + ")";
+        } else {
+            document.getElementById("yearlvl_p").style.display = "none";
+            document.getElementById("section_p").style.display = "none";
+            get(child(dbRef, "PARSEIT/administration/teachers/" + id)).then((snapshot) => {
+                if (snapshot.exists()) {
+                    document.getElementById("confirm-id").innerText = snapshot.val().id;
+                    document.getElementById("confirm-fullname").innerText = localStorage.getItem("name-parser");
+                    document.getElementById("confirm-username").innerText = username;
+                    document.getElementById("confirm-birthday").innerText = snapshot.val().birthday;
+                    document.getElementById("confirm-email").innerText = localStorage.getItem("email-parser");
+                }
+            });
+        }
+    })
+}
+
+function getYearType(year) {
+    if (year === "1") {
+        return "Freshman"
+    }
+    if (year === "2") {
+        return "Sophomore"
+    }
+    if (year === "3") {
+        return "Junior"
+    }
+    if (year === "4") {
+        return "Senior"
+    }
+}
+
+function checkRegular(reg) {
+    if (reg === "yes") {
+        return "Regular"
+    }
+    else {
+        return "Irregular"
+    }
+}
+
+function createParser(email, password, id, username) {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            updateParser(id, type, username);
+        })
+        .catch((error) => {
+            // Handle errors
+            console.error("Error signing up:", error.code, error.message);
+        });
+
+
+}
+
+function updateParser(id, type, username) {
+    if (type === "student") {
+        update(ref(database, "PARSEIT/administration/students/" + id), {
+            activated: "yes",
+        }).then(() => {
+            set(ref(database, "PARSEIT/username/" + username), id).then(() => {
+                localStorage.removeItem("email-parser");
+                localStorage.removeItem("name-parser");
+                localStorage.removeItem("activate-parser");
+                localStorage.removeItem("type-parser");
+                localStorage.setItem("user-parser", id);
+                window.location.href = "homepage.html";
+            });
+        });
+    } else {
+        update(ref(database, "PARSEIT/administration/teachers/" + id), {
+            activated: "yes",
+        }).then(() => {
+            set(ref(database, "PARSEIT/username/" + username), id).then(() => {
+                localStorage.removeItem("email-parser");
+                localStorage.removeItem("name-parser");
+                localStorage.removeItem("activate-parser");
+                localStorage.removeItem("type-parser");
+                localStorage.setItem("user-parser", id);
+                window.location.href = "homepage.html";
+            });
+        });
+    }
+
 }
--- homepage.js
+++ homepage.js
@@ -2,3 +2,4 @@ function signout() {
     localStorage.removeItem("user-parser");
     window.location.href = "login.html";
 }
+document.getElementById("username").innerText = localStorage.getItem("user-parser");
