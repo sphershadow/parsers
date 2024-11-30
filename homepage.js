3ba721a0c64f8903fe16a8ea92e695e1a1d8ad26
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-30 22:20:23 +0800

    update nave

--- homepage.js
+++ homepage.js
@@ -37,3 +37,172 @@ function logout() {
     localStorage.removeItem("user-parser");
     window.location.href = "login.html";
 }
+
+document.getElementById("homelobby_btn").addEventListener("click", function () {
+    showBodyWrapper("home_all_sec");
+    selectNavIcon("homelobby_img");
+    selectNavLbl("homelobby_lbl");
+
+    //revert
+    hideBodyWrapper("game_student_sec");
+    revertNavIcon("homegame_img");
+    revertNavLbl("homegame_lbl");
+
+    hideBodyWrapper("library_student_sec");
+    revertNavIcon("homelibrary_img");
+    revertNavLbl("homelibrary_lbl");
+
+    hideBodyWrapper("chatgpt_all_sec");
+    revertNavIcon("homechatbot_img");
+    revertNavLbl("homechatbot_lbl");
+});
+document.getElementById("homegame_btn").addEventListener("click", function () {
+    showBodyWrapper("game_student_sec");
+    selectNavIcon("homegame_img");
+    selectNavLbl("homegame_lbl");
+
+    //revert
+    hideBodyWrapper("home_all_sec");
+    revertNavIcon("homelobby_img");
+    revertNavLbl("homelobby_lbl");
+
+    hideBodyWrapper("library_student_sec");
+    revertNavIcon("homelibrary_img");
+    revertNavLbl("homelibrary_lbl");
+
+    hideBodyWrapper("chatgpt_all_sec");
+    revertNavIcon("homechatbot_img");
+    revertNavLbl("homechatbot_lbl");
+});
+document.getElementById("homelibrary_btn").addEventListener("click", function () {
+    showBodyWrapper("library_student_sec");
+    selectNavIcon("homelibrary_img");
+    selectNavLbl("homelibrary_lbl");
+
+    //revert
+    hideBodyWrapper("home_all_sec");
+    revertNavIcon("homelobby_img");
+    revertNavLbl("homelobby_lbl");
+
+    hideBodyWrapper("game_student_sec");
+    revertNavIcon("homegame_img");
+    revertNavLbl("homegame_lbl");
+
+    hideBodyWrapper("chatgpt_all_sec");
+    revertNavIcon("homechatbot_img");
+    revertNavLbl("homechatbot_lbl");
+});
+document.getElementById("homechatbot_btn").addEventListener("click", function () {
+    showBodyWrapper("chatgpt_all_sec");
+    selectNavIcon("homechatbot_img");
+    selectNavLbl("homechatbot_lbl");
+
+    //revert
+    hideBodyWrapper("home_all_sec");
+    revertNavIcon("homelobby_img");
+    revertNavLbl("homelobby_lbl");
+
+    hideBodyWrapper("game_student_sec");
+    revertNavIcon("homegame_img");
+    revertNavLbl("homegame_lbl");
+
+    hideBodyWrapper("library_student_sec");
+    revertNavIcon("homelibrary_img");
+    revertNavLbl("homelibrary_lbl");
+});
+
+document.getElementById("homelobby_btnx").addEventListener("click", function () {
+    showBodyWrapper("home_all_sec");
+    selectNavIcon("homelobby_imgx");
+    selectNavLbl("homelobby_lblx");
+
+    //revert
+    hideBodyWrapper("honors_teacher_sec");
+    revertNavIcon("homehonors_img");
+    revertNavLbl("homehonors_lbl");
+
+    hideBodyWrapper("share_teacher_sec");
+    revertNavIcon("homeshare_img");
+    revertNavLbl("homeshare_lbl");
+
+    hideBodyWrapper("chatgpt_all_sec");
+    revertNavIcon("homechatbot_imgx");
+    revertNavLbl("homechatbot_lblx");
+});
+
+document.getElementById("homehonors_btn").addEventListener("click", function () {
+    showBodyWrapper("honors_teacher_sec");
+    selectNavIcon("homehonors_img");
+    selectNavLbl("homehonors_lbl");
+
+    //revert
+    hideBodyWrapper("home_all_sec");
+    revertNavIcon("homelobby_imgx");
+    revertNavLbl("homelobby_lblx");
+
+    hideBodyWrapper("share_teacher_sec");
+    revertNavIcon("homeshare_img");
+    revertNavLbl("homeshare_lbl");
+
+    hideBodyWrapper("chatgpt_all_sec");
+    revertNavIcon("homechatbot_imgx");
+    revertNavLbl("homechatbot_lblx");
+});
+
+document.getElementById("homeshare_btn").addEventListener("click", function () {
+    showBodyWrapper("share_teacher_sec");
+    selectNavIcon("homeshare_img");
+    selectNavLbl("homeshare_lbl");
+
+    //revert
+    hideBodyWrapper("home_all_sec");
+    revertNavIcon("homelobby_imgx");
+    revertNavLbl("homelobby_lblx");
+
+    hideBodyWrapper("honors_teacher_sec");
+    revertNavIcon("homehonors_img");
+    revertNavLbl("homehonors_lbl");
+
+    hideBodyWrapper("chatgpt_all_sec");
+    revertNavIcon("homechatbot_imgx");
+    revertNavLbl("homechatbot_lblx");
+});
+
+document.getElementById("homechatbot_btnx").addEventListener("click", function () {
+    showBodyWrapper("chatgpt_all_sec");
+    selectNavIcon("homechatbot_imgx");
+    selectNavLbl("homechatbot_lblx");
+
+
+    //revert
+    hideBodyWrapper("home_all_sec");
+    revertNavIcon("homelobby_imgx");
+    revertNavLbl("homelobby_lblx");
+
+    hideBodyWrapper("honors_teacher_sec");
+    revertNavIcon("homehonors_img");
+    revertNavLbl("homehonors_lbl");
+
+    hideBodyWrapper("share_teacher_sec");
+    revertNavIcon("homeshare_img");
+    revertNavLbl("homeshare_lbl");
+});
+
+function showBodyWrapper(id) {
+    document.getElementById(id).style.display = "flex";
+}
+function selectNavIcon(id) {
+    document.getElementById(id).style.filter = "invert(30%) sepia(59%) saturate(7076%) hue-rotate(350deg) brightness(88%) contrast(120%)";
+}
+function selectNavLbl(id) {
+    document.getElementById(id).style.color = "#f30505";
+}
+function hideBodyWrapper(id) {
+    document.getElementById(id).style.display = "none";
+}
+function revertNavIcon(id) {
+    document.getElementById(id).style.filter = "invert(54%) sepia(8%) saturate(27%) hue-rotate(319deg) brightness(91%) contrast(85%)";
+}
+function revertNavLbl(id) {
+    document.getElementById(id).style.color = "#808080";
+}
