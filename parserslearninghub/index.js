81c703fc6f785f27a1936ba83ce8d43c728a5f5e
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-12-15 13:20:10 +0800

    animation join

--- parserslearninghub/index.js
+++ parserslearninghub/index.js
@@ -6,7 +6,45 @@ document.getElementById("home_btn").addEventListener("click", function () {
   window.location.href = "../homepage.html";
 });
 
+$(document).ready(function () {
+  const page = $('#entercode_div');
+  $('#joinquiz_btn').click(function () {
+    page.show();
+    anime({
+      targets: page[0],
+      clipPath: ['circle(0% at 50% 50%)', 'circle(100% at 50% 50%)'],
+      duration: 400,
+      easing: 'easeInOutQuad',
+
+    });
+  });
+});
+
+$(document).ready(function () {
+  const page = $('#entercode_div');
+  $('#returnjoinquiz_btn').click(function () {
+    anime({
+      targets: page[0],
+      clipPath: ['circle(90% at 50% 50%)', 'circle(0% at 50% 50%)'],
+      duration: 400,
+      easing: 'easeInOutQuad',
+    });
+  });
+});
+
+
 function setScreenSize(width, height) {
   document.body.style.width = width + "px";
   document.body.style.height = height + "px";
+}
+
+function circleAnimation(page) {
+  page.show();
+  anime({
+    targets: page[0],
+    clipPath: ['circle(0% at 50% 50%)', 'circle(100% at 50% 50%)'],
+    duration: 400,
+    easing: 'easeInOutQuad',
+
+  });
 }
